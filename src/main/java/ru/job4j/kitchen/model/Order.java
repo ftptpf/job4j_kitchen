f0cf9cb4a02ea0a3da4f941f2bd5bf86f43f3251
ftptpf <inboxru@yandex.ru>
2023-05-31 09:40:13 +0300

    Add logging properties

--- src/main/java/ru/job4j/kitchen/model/Order.java
+++ src/main/java/ru/job4j/kitchen/model/Order.java
@@ -2,18 +2,10 @@ package ru.job4j.kitchen.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.io.Serializable;
 
-@Entity(name = "job4j_order")
 @Data
-public class Order {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Order implements Serializable {
     private int id;
-
     private int dishId;
 }
